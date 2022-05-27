80cf0e2f7bd40dcd87bd348fcb07d8e6fade680e
moksh-rambhiya <106058384+moksh-rambhiya@users.noreply.github.com>
2022-05-27 12:44:22 +0530

    Add files via upload

---
+++ Squares.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main (){
+int a=2 , b=5 , c=9;
+
+a=a*a;
+b=b*b;
+printf("%d\t%d\n",a,b);
+a=a*2;
+}
+// Same for 5 and 9
---
+++ poqwer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+void main () {
+    int n , z;
+    printf("Please enter a number\n");
+    scanf("%d",&n);
+    z=pow(n,2);
+    printf("The perfect square is %d\n",z);
+}
