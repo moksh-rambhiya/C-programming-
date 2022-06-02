2a9eed1fb95e9e9a86b60fc757d1b81d651cdadf
moksh-rambhiya <106058384+moksh-rambhiya@users.noreply.github.com>
2022-06-02 10:43:51 +0530

    Add files via upload

---
+++ 3 digit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main () {
+int a,b,c,d;
+printf("Please enter a three digit number\n");
+
+scanf("%d %d %d",&a,&b,&c);
+
+printf("%d is at hundreds place\n%d is at tens place\n%d is at units place\n",a,b,c);
+}
